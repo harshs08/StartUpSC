fae6f1ed719c5d9549c7e6d41b679b98979cf3e7
Harsh Singh <harshsin@usc.edu>
2014-03-14 16:00:19 -0700

    route setup to point to index file

---
+++ app/routes.js
@@ -0,0 +1,21 @@
+
+module.exports = function(app) {
+
+    // server routes
+    // handle things like api calls
+    // authentication routes
+
+    // sample api route
+    app.get('/api/demo', function(req, res) {
+
+    });
+
+
+    // frontend routes
+
+    // route to handle all angular requests
+    app.get('*', function(req, res) {
+        res.sendfile('./public/views/index.html'); // load public/views/index.html file
+    });
+
+};
