8e24c55dabba8adb43a73eabba54c6923fde2249
Harsh Singh <harshsin@usc.edu>
2014-03-14 19:09:44 -0700

    added user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,48 @@
 /**
- * Created with JetBrains WebStorm.
  * User: harshsingh
  * Date: 3/14/14
  * Time: 4:52 PM
- * To change this template use File | Settings | File Templates.
  */
+
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+
+    local            : {
+        fname        : String,
+        lname        : String,
+        email        : String,
+        password     : String
+    },
+    facebook         : {
+        fname        : String,
+        lname        : String,
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    twitter          : {
+        fname        : String,
+        lname        : String,
+        id           : String,
+        token        : String,
+        displayName  : String,
+        username     : String
+    }
+
+});
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// password check
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users
+module.exports = mongoose.model('User', userSchema);
