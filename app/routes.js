8cfa770f798748c2f407129276e2a9798aa14a76
Harsh Singh <harshsin@usc.edu>
2014-03-14 19:09:00 -0700

    added routes for login, signup and profile view

--- app/routes.js
+++ app/routes.js
@@ -4,19 +4,57 @@
  * Time: 4:05 PM
 
  */
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
     // server routes
     // handle things like api calls
     // authentication routes
 
-    // sample api route
-    app.get('/api/demo', function(req, res) {
-
+    // Login Form
+    app.get('/login', function(req, res) {
+        res.render('login.html', { message: req.flash('loginMessage') });
     });
 
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-    // frontend routes
+
+    // Signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.html', { message: req.flash('signupMessage') });
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    //Profile view
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.html', {
+            user : req.user // get the user in session
+        });
+    });
+
+    //Posting view
+    app.get('/posting', isLoggedIn, function(req, res) {
+        res.render('posting.html', {
+            posting : req.posting // get the posting
+        });
+    });
+
+    //logout
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
 
     // route to handle all angular requests
     app.get('*', function(req, res) {
@@ -24,3 +62,14 @@ module.exports = function(app) {
     });
 
 };
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
