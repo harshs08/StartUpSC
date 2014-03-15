db8fe7228e1ca0ba237249609edeab0141c2c6b5
Harsh Singh <harshsin@usc.edu>
2014-03-14 19:45:16 -0700

    passport.js cleanup

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,14 @@
 // config/passport.js
 
 // load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy  = require('passport-local').Strategy;
 
 // load up the user model
-var User       		= require('../app/models/user');
+var User = require('../app/models/user');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
@@ -27,12 +22,7 @@ module.exports = function(passport) {
         });
     });
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
+    //Local SignUp
     passport.use('local-signup', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
             usernameField : 'email',
@@ -75,12 +65,8 @@ module.exports = function(passport) {
 
         }));
 
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
 
+    //Local SignIn
     passport.use('local-login', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
             usernameField : 'email',
