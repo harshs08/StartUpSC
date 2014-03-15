ec8990f471bf8b5e5a1413d214e285bf140f94aa
Harsh Singh <harshsin@usc.edu>
2014-03-14 19:07:11 -0700

    Modified to add local signin

---
+++ config/passport.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: harshsingh
+ * Date: 3/14/14
+ * Time: 6:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- server.js
+++ server.js
@@ -5,25 +5,45 @@
  */
 
 // load modules
-var express = require('express');
-var app     = express();
-var mongoose= require('mongoose');
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash 	 = require('connect-flash');
+
 
 // config
-var db = require('./config/db');
+var db = require('./config/db.js');
+
+//connect to db
+mongoose.connect(db.url, function(err) {
+    if (err) throw err;
+});
+
+
+// pass passport for configuration
+require('./config/passport')(passport);
 
 //set port
 var port = process.env.PORT || 8080;
 
+
 app.configure(function() {
-    app.use(express.static(__dirname + '/public')); 	// set the static files
-    app.use(express.logger('dev')); 					// logging
-    app.use(express.bodyParser()); 						// have the ability to pull information from html in POST
-    app.use(express.methodOverride()); 					// simulate DELETE and PUT
+    app.set('views', __dirname + '/public/views');
+    app.engine('html', require('ejs').renderFile);
+    app.use(express.static(__dirname + '/public'));    // set the static files
+    app.use(express.logger('dev')); 			      // logging
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser());                   // have the ability to pull information from html in POST
+    app.use(express.methodOverride()); 			    // simulate DELETE and PUT
+    app.use(express.session({ secret: 'godconnection' })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
 });
 
 // configure routes
-require('./app/routes')(app);
+require('./app/routes.js')(app, passport);
 
 //start server and logging
 app.listen(port);						  // startup our app at http://localhost:8080
